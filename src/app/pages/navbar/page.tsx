e7c2e6bc2d1be0c5665f0fbdbc3bc89690b99741
Cristian Muyudumbay <cristian.muyudumbay@gmail.com>
2024-05-19 19:01:16 -0500

    fixed navbar

--- src/app/pages/navbar/page.tsx
+++ src/app/pages/navbar/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 import {
+  Box,
   Button,
   Flex,
   Heading,
+  IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverContent,
+  PopoverTrigger,
+  Select,
   Spacer,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 const Navbar = () => {
   return (
@@ -31,45 +41,90 @@ const Navbar = () => {
 
       <Spacer />
 
-      <Flex align="center" justify="flex-end">
-        <Link href="/">
-          <Button colorScheme="teal" mr={4}>
-            Inicio
-          </Button>
-        </Link>
-        <Link href="/pages/mapbox">
-          <Button colorScheme="teal" mr={4}>
-            Map Box
-          </Button>
-        </Link>
-        <Link href="/pages/rti">
-          <Button colorScheme="teal" mr={4}>
-            Tabla Ag Grid
-          </Button>
-        </Link>
-        <Link href="/pages/chartAggrid">
-          <Button colorScheme="teal" mr={4}>
-            Grafica
-          </Button>
-        </Link>
-        <Menu>
-          <MenuButton as={Button} colorScheme="teal">
-            Anime
-          </MenuButton>
-          <MenuList>
-            <MenuItem color={"black"}>
-              <Link href="/pages/anime" color="black">
-                Anime
-              </Link>
-            </MenuItem>
-            <MenuItem color={"black"}>
-              <Link href="/pages/animeOpenings" color="black">
-                Anime Op
-              </Link>
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex>
+      <div className="lg:hidden">
+        <Flex align="center" justify="flex-end">
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+            />
+            <MenuList color={"black"}>
+              <MenuItem>
+                <Link href={"/"}>Inicio</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href={"/pages/mapbox"}>Map Box</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href={"/pages/rti"}>Tabla Ag Grid</Link>
+              </MenuItem>
+              <MenuItem>
+                <Link href={"/pages/chartAggrid"}>Grafica</Link>
+              </MenuItem>
+              <MenuItem>
+                <Popover trigger="hover" placement="right-start">
+                  <PopoverTrigger>
+                    <Box as="span" cursor="pointer">
+                      Anime
+                    </Box>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverBody className="flex flex-col">
+                      <Link href={"/pages/anime"}>Anime Top</Link>
+                      <Link href={"/pages/animeOpenings"}>Youtube</Link>
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </div>
+
+      <div className="hidden lg:inline-flex">
+        <Flex align="center" justify="flex-end">
+          <Link href="/">
+            <Button colorScheme="teal" mr={4}>
+              Inicio
+            </Button>
+          </Link>
+          <Link href="/pages/mapbox">
+            <Button colorScheme="teal" mr={4}>
+              Map Box
+            </Button>
+          </Link>
+          <Link href="/pages/rti">
+            <Button colorScheme="teal" mr={4}>
+              Tabla Ag Grid
+            </Button>
+          </Link>
+          <Link href="/pages/chartAggrid">
+            <Button colorScheme="teal" mr={4}>
+              Grafica
+            </Button>
+          </Link>
+          <Menu>
+            <MenuButton as={Button} colorScheme="teal">
+              Anime
+            </MenuButton>
+            <MenuList>
+              <MenuItem color={"black"}>
+                <Link href="/pages/anime" color="black">
+                  Anime
+                </Link>
+              </MenuItem>
+              <MenuItem color={"black"}>
+                <Link href="/pages/animeOpenings" color="black">
+                  Anime Op
+                </Link>
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </div>
     </Flex>
   );
 };
